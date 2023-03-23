cb15ff51814c7558b93a05980ad4fe2fdc8a024d
lkra <47949097+lkra@users.noreply.github.com>
2023-03-23 17:50:32 +0100

    add godel approach

---
+++ CLTeamL/approaches/GODEL/dstc11/dstc11_dataset.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+#  coding=utf-8
+#  Copyright (c) Microsoft Corporation.
+#  Licensed under the MIT license.
+
+"""Corpus for DSTC11"""
+
+import datasets
+import jsonlines
+
+
+_DESCRIPTION = """\
+DSTC11
+"""
+
+_CITATION = """\
+DSTC11
+"""
+
+_WEBPAGE = ""
+
+
+class DSTC11(datasets.GeneratorBasedBuilder):
+    """DSTC11"""
+
+    def _info(self):
+        return datasets.DatasetInfo(
+            description=_DESCRIPTION,
+            features=datasets.Features(
+                {
+                    "Context": datasets.Value("string"),
+                    "Response": datasets.Value("string"),
+                    "Knowledge": datasets.Value("string"),
+                }
+            ),
+            homepage=_WEBPAGE,
+            citation=_CITATION,
+        )
+
+    def _split_generators(self, dl_manager):
+
+        train_path = '../examples/dstc11/dstc11_train.jsonl'
+        validation_path = '../examples/dstc11/dstc11_val.jsonl'
+        test_path = '../examples/dstc11/dstc11_val.jsonl'
+        return [
+            datasets.SplitGenerator(name=datasets.Split.TRAIN, gen_kwargs={
+                                    "filepath": train_path}),
+            datasets.SplitGenerator(name=datasets.Split.VALIDATION, gen_kwargs={
+                                    "filepath": validation_path}),
+            datasets.SplitGenerator(name=datasets.Split.TEST, gen_kwargs={
+                                    "filepath": test_path}),
+        ]
+
+    def _generate_examples(self, filepath):
+
+        key = 0
+        with open(filepath, "r", encoding="utf-8") as reader:
+
+            for item in jsonlines.Reader(reader):
+                yield key, item
+                key += 1
