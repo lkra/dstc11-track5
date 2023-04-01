79f3d2fb43ed7f6c793a0e03885168e3e3b91c7b
lkra <47949097+lkra@users.noreply.github.com>
2023-04-01 02:16:01 +0200

    changed ks training parameters

---
+++ bin/run_baseline_training_ks_5.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# training and validation for knowledge selection
+model_name=microsoft/deberta-v3-base
+model_name_exp=deberta-v3-base
+cuda_id=0
+
+CUDA_VISIBLE_DEVICES=${cuda_id} python baseline.py \
+        --task selection \
+        --dataroot data \
+        --model_name_or_path ${model_name} \
+        --negative_sample_method "oracle" \
+        --knowledge_file knowledge.json \
+        --params_file baseline/configs/selection/params_5.json \
+        --exp_name ks-review-${model_name_exp}-oracle-baseline
+
