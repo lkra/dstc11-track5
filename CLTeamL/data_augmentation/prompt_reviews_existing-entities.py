ef89145570aaadfb483cd99ab3ad02c44dd8a04b
Lea <47949097+lkra@users.noreply.github.com>
2023-07-25 15:25:40 +0200

    Update prompt_reviews_existing-entities.py

--- CLTeamL/data_augmentation/prompt_reviews_existing-entities.py
+++ CLTeamL/data_augmentation/prompt_reviews_existing-entities.py
@@ -6,10 +6,8 @@ import traceback
 import openai
 from dotenv import load_dotenv
 
-openai.api_key = 'sk-***REMOVED***'
-
-# load_dotenv()
-# openai.api_key = os.getenv("OPENAI_API_KEY")
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
 # process entity reviews
